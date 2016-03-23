58c0be8a445f3e8b74eb779526ff61d5c4a28ed4
geeac <eugenia.cosinschi@gmail.com>
2016-03-23 09:51:43 +0200

    cta
    cta box

--- content-page.php
+++ content-page.php
@@ -8,6 +8,28 @@
  */
 ?>
 
+<?php if ( is_front_page() ):
+
+	$cta_content = get_post_meta( get_the_ID(), 'cta_content', true );
+	$cta_button_text = get_post_meta( get_the_ID(), 'cta_button_text', true );
+	$cta_button_url = get_post_meta( get_the_ID(), 'cta_button_url', true );
+	$cta_button_color = get_post_meta( get_the_ID(), 'cta_button_color', true );
+	$cta_background_color = get_post_meta( get_the_ID(), 'cta_background_color', true );
+	$cta_text_color = get_post_meta( get_the_ID(), 'cta_text_color', true );
+
+?>
+
+	<div class="cta-container" style="background-color:<?php echo $cta_background_color; ?>">
+		<p class="cta-content" style="color:<?php echo $cta_text_color; ?>"><?php echo $cta_content; ?></p>
+		<a href="<?php echo $cta_button_url; ?>" class="cta-button" style="background-color:<?php echo $cta_button_color; ?>"><?php echo $cta_button_text; ?></a>
+	</div>
+
+<?php
+
+	endif;
+
+?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
